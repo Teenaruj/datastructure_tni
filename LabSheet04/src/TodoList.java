58f8de62b03f56402b90993bacf4110d55baddef
Teenaruj <106090783+Teenaruj@users.noreply.github.com>
2026-07-27 22:05:39 +0700

    Update TodoList.java

--- LabSheet04/src/TodoList.java
+++ LabSheet04/src/TodoList.java
@@ -19,6 +19,17 @@ public class TodoList {
 		
 		todoList.insert("Go to gym");
 		System.out.println(todoList.traversal());
+
+		todoList.remove();
+		System.out.println("To-Do List = " + todoList.traversal());
+
+		todoList.remove(3);
+		System.out.println("To-Do List = " + todoList.traversal());
+
+		todoList.remove(0);
+		System.out.println("To-Do List = " + todoList.traversal());
+
+		System.out.println("\nBackward Traversal " + todoList.backwardTraversal());
 	}
 
 }
