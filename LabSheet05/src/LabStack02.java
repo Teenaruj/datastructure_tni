ad8a7ad2ca5d74b2ab1aafb1b55a451f5230cd13
Teenaruj <106090783+Teenaruj@users.noreply.github.com>
2026-07-31 11:16:47 +0700

    LabSheet05

---
+++ LabSheet05/src/LabStack02.java
@@ -0,0 +1,21 @@
+import java.util.Stack;
+public class LabStack02 {
+ 
+	public static void main(String[] args) {
+		Stack<Integer> stacks = new Stack<>();
+	
+	for (int i=0; i<5; i++) {
+		System.out.println ("Push in track :"+(i+1));
+		stacks.push(i+1);
+	}
+	System.out.println("Train car after into dead-end track : " + stacks.toString() + "\n");
+	
+	while(!stacks.isEmpty()) {
+		System.out.println("Pop from track : " + stacks.peek());
+		stacks.pop();
+	}
+	System.out.println("Train car after out of dead-end track : " + stacks.toString() + "\n");
+	}
+}
+ 
+ 
