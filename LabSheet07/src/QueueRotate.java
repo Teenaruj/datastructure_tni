bdfa20be8ef08c17e0bf94c9207fcb8406f15f43
Teenaruj <106090783+Teenaruj@users.noreply.github.com>
2026-08-14 11:02:29 +0700

    QueueRotate.java
    Refactored queue rotation logic and improved input handling.

--- LabSheet07/src/QueueRotate.java
+++ LabSheet07/src/QueueRotate.java
@@ -1,43 +1,48 @@
+
 import java.util.ArrayDeque;
 import java.util.Queue;
 import java.util.Scanner;
 
 public class QueueRotate {
-	public static Queue<Integer> queue = new ArrayDeque<Integer>();
-
-	public static void main(String[] args) {
-
-		Scanner input = new Scanner(System.in);
-
-		originalQueue();
-		System.out.println("Queue => " + queue);
-
-		while (true) {
-			System.out.println();
-			System.out.print("Press 1 to rotate queue: ");
-			int choice = input.nextInt();
-
-			if (choice == 1) {
-				System.out.println("Calling queue: " + queue.peek());
-				rotateQueue();
-				System.out.println("Queue => " + queue);
-			} else {
-				System.out.println("Exit");
-				break;
-			}
-		}
-
-	}
-
-	public static void rotateQueue() {
-		int front = queue.poll();
-		queue.offer(front);
-	}
-
-	public static void originalQueue() {
-		for (int value = 101; value <= 107; value++) {
-			queue.offer(value);
-		}
-	}
-
+    public static Queue<Integer> queue = new ArrayDeque<Integer>();
+    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        originalQueue();
+        
+        while (true) {
+            System.out.print("Press 1 to rotate queue: ");
+            if (input.hasNextInt()) {
+                int choice = input.nextInt();
+                if (choice == 1) {
+                    rotateQueue();
+                } else {
+                    break;
+                }
+            } else {
+                break;
+            }
+        }
+        
+        System.out.println("Exit");
+        input.close();
+    }
+    
+    public static void rotateQueue() {
+        if (!queue.isEmpty()) {
+            int current = queue.poll();
+            System.out.println("Calling queue: " + current);
+            queue.add(current);
+            System.out.println("Queue => " + queue);
+        }
+    }
+    
+    public static void originalQueue() {
+        queue.clear();
+        for (int i = 101; i <= 107; i++) {
+            queue.add(i);
+        }
+        System.out.println("Queue => " + queue);
+    }
 }
